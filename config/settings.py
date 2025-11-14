642b037b19d2e83c84d9e16d762983022268bc04
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2025-11-14 13:48:51 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -72,7 +72,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'config.middleware.swagger_auto_bearer',
 ]
 
 ROOT_URLCONF = 'config.urls'
