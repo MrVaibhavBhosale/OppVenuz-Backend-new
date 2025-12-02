e4b6a32e6d93b6c2801acf8b730da29f373c33ad
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2025-12-02 18:04:04 +0530

    Add user registration URL to urlpatterns

--- user/urls.py
+++ user/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path
 from .views import (
     ImageUploadView,
+    UserRegistrationView,
 )
 
 urlpatterns = [
     path("v1/uploadImageToS3", ImageUploadView.as_view(), name="upload-image-to-s3"),
+    path("userregister/", UserRegistrationView.as_view(), name="user-register"),
+ 
+    
 ]
