132a9a7908f0ace310a067fcbd5f5c87113bd12c
Vaibhav Bhosale <106335135+MrVaibhavBhosale@users.noreply.github.com>
2025-12-04 16:23:48 +0530

    Update settings.py

--- config/settings.py
+++ config/settings.py
@@ -43,7 +43,7 @@ REST_FRAMEWORK = {
 
 # Simple JWT settings
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(hours=1),
+    'ACCESS_TOKEN_LIFETIME': timedelta(hours=8),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=7),
     'ROTATE_REFRESH_TOKENS': True,
     'BLACKLIST_AFTER_ROTATION': True,
